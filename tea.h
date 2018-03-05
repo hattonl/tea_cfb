552f1170dab7c054d5cdc301a0ca643a9ed00016
hattonl <hattonl@163.com>
2018-03-06 00:39:32 +0800

    Create tea.h

---
+++ tea.h
@@ -0,0 +1,15 @@
+#ifndef __TEA_H
+#define __TEA_H
+
+#ifndef u8
+#define u8 unsigned char
+#endif
+#ifndef u16
+#define u16 unsigned short
+#endif
+#ifndef u32
+#define u32 unsigned int
+#endif
+
+
+#endif
